c87bca80a27cb85b9d41172fbedabb899364c12b
Esa Rijal Mustaqbal <Esa_RM963@mitrais.com>
2018-10-26 16:59:03 +0700

    finishing react tutorial newbie

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Game from './Game'
+import LoginControl from './LoginControl'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Calculator from './Calculator'
+import FilterableProductTable from './searchbox/SearchBox'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ========================================
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(
+  
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('root')
+);
